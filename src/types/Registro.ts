ec343cfc6e76d55da9a35a5e4ca8b85910ce0cdb
Pedrojoser <pedritho11012004@gmail.com>
2026-01-29 10:38:26 -0500

    Agregando contenido

---
+++ src/types/Registro.ts
@@ -0,0 +1,11 @@
+/** Definición del tipo Registro para representar la información de un registro de estudiante */
+export type Registro = {
+  nombre: string;
+  correo: string;
+  edad: number;
+  telefono: string;
+  documento: string;
+  genero: string;
+  carrera: string;
+};
+
